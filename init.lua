adf8a8833389ef3721392242595d461a6ffaa242
vagrant <vagrant@kali.localdomain>
2022-12-19 12:54:36 -0500

    Update

--- init.lua
+++ init.lua
@@ -102,28 +102,29 @@ require("packer").startup(function(use)
     use("tpope/vim-repeat")
     use("ggandor/leap.nvim")
     use("nvim-neorg/neorg")
+    use { "shortcuts/no-neck-pain.nvim", tag = "*" }
 
     use {
-  'VonHeikemen/lsp-zero.nvim',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},
-    {'rafamadriz/friendly-snippets'},
-  }
-}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
 
     local luasnip = require("luasnip")
@@ -266,8 +267,13 @@ require("packer").startup(function(use)
         options = vim.tbl_extend("force", { match = "§§", matchStrategy = "exact" }, options or {})
         require("zk").edit(options, { title = "§§" })
     end)
-    require('neogen').setup({ snippet_engine = "luasnip", languages = { lua = { annotation_convention = "ldoc"}}})
+    require('neogen').setup({ snippet_engine = "luasnip", languages = { lua = { annotation_convention = "ldoc" } } })
     require("todo-comments").setup {}
     require("leap").add_default_mappings()
+    require("no-neck-pain").setup({
+        buffers = {
+            background = { colorCode = "rose-pine-moon" }
+        }
+    })
 
 end)
